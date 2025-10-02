401d2b735d1d9ad320e22090f3df909c322784fe
tomamic <tomamic@ce.unipr.it>
2025-10-02 11:35:10 +0200

    linearity

---
+++ c03_linearity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - https://tomamic.github.io/
+@license This software is free - https://opensource.org/license/mit
+"""
+
+n = int(input("Rounds? "))  # try with 5
+
+first = float(input("First value? "))  # try with 10
+last = float(input("Last value? "))    # try with 15
+
+delta = (last - first) / (n - 1)
+print("Delta :", delta)
+
+for i in range(n):
+    v = i * delta + first
+    print("Round", i + 1, ":", v)
