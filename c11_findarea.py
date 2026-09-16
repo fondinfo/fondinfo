55658c437d6c5ffaf3d4fc86d9ac8f9e45a1f9ba
tomamic <tomamic@ce.unipr.it>
2026-09-16 17:15:45 +0200

    findarea

--- c11_findarea.py
+++ c11_findarea.py
@@ -1,21 +1,28 @@
 def find_area(board, x, y, val, area=None):
-    if not area:
+    if area is None:
         area = set()
-    h, w = len(board), len(board[0])
-    if 0 <= x < w and 0 <= y < h and board[y][x] == val and (x, y) not in area:
-        area.add((x, y))
+    if y < 0 or y >= len(board) or x < 0 or x >= len(board[0]):
+        return area
+
+    if board[y][x] == val and (x, y) not in area:
+        area.add((x, y))  # area is a set of points
         for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
             find_area(board, x + dx, y + dy, val, area)
     return area
-        
-        
-board = [[0, 1, 0, 0],
-         [0, 1, 1, 0],
-         [1, 0, 1, 1],
-         [0, 0, 0, 0]]
 
-x = int(input("x? "))
-y = int(input("y? "))
-val = int(input("val? "))
-area = find_area(board, x, y, val)
-print(area)
+
+def main():
+    board = [[0, 1, 0, 0],
+             [0, 1, 1, 0],
+             [1, 0, 1, 1],
+             [0, 0, 0, 0]]
+    w, h = 4, 4
+
+    x = int(input("x? "))
+    y = int(input("y? "))
+    v = int(input("v? "))
+    area = find_area(board, x, y, v)
+    print(area)
+
+if __name__ == "__main__":
+    main()
